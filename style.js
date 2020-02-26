6d1a3520ede5d0f68a60abcac54c25e26f7d82a6
jleesecond <newton16@msu.edu>
2020-02-26 11:36:14 -0500

    btnKick returns an error no matter what. I've googled it, but the only way to addEventListener on the non declaring lines is to have it in html doc

--- style.js
+++ style.js
@@ -1,10 +1,25 @@
 
+var audio_kick = document.getElementById('audio-kick')
+var btnKick = document.getElementById('button-kick').addEventListener('click', function Player(){console.log('first'); audio_kick.play()});
+btnKick.addEventListener('mouseenter', function Player(){console.log('event 2'); audio_kick.play()});
+var btnClap = document.getElementById('button-clap');
+var btnHat = document.getElementById('button-hihat');
 
-function Press(note){
-  var drum1 = new Audio();
-  var path = "../";
-  var snd = new Audio(path + note + ".wav");
-  return snd.play();
+// btnKick.addEventListener('mouseenter', Press(note));
+
+
+function Player(event){
+  if(event.Play()){
+    console.log("made it into Player function if statement")
+    return 0;
+  }
+  else{
+    console.log('Else Statement')
+    event.Play();
+    return 0;
+  }
+
 }
-Press();
+
+
 
